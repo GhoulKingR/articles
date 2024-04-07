59693410b0edab1f54191df26bc174f9a945905d
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-04-07 11:56:56 +0100

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+import Avatar from "./assets/images/avatar-jessica.jpeg";
+
+function App() {
+  const links = [
+    {name: "GitHub"},
+    {name: "Frontend Mentor"},
+    {name: "LinkedIn"},
+    {name: "Twitter"},
+    {name: "Instagram"},
+  ];
+
+  return (
+    <Container>
+      <div>
+        <img src={Avatar} alt="avatar" />
+        <h1>Jessica Randall</h1>
+        <h2>London, United Kingdom</h2>
+        <p>"Front-end developer and avid reader."</p>
+        <ul>
+          {
+            links.map((v, i) => (
+              <li key={i}>{v.name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </Container>
+  )
+}
+
+export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #141414;
+  
+  & > div {
+    width: calc(100vw - 48px);
+    max-width: calc(384px - 48px);
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    padding: 24px;
+    background-color: #1f1f1f;
+    border-radius: 12px;
+
+    & > img {
+      width: 88px;
+      height: 88px;
+      border-radius: 44px;
+      margin-bottom: 24px;
+    }
+
+    & > h1 {
+      font-weight: bold;
+      line-height: 150%;
+      font-size: 24px;
+      letter-spacing: 0%;
+      margin-bottom: 4px;
+    }
+
+    & > h2 {
+      color: #C4F82A;
+      font-weight: bold;
+      font-size: 14px;
+      line-height: 150%;
+      letter-spacing: 0%;
+      margin-bottom: 24px;
+    }
+    
+    & > p {
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 150%;
+      letter-spacing: 0%;
+      margin-bottom: 24px;
+    }
+
+    & > ul {
+      list-style-type: none;
+      width: 100%;
+      margin-bottom: -16px;
+
+      & > li {
+        background-color: #333333;
+        width: calc(inherit - 24px);
+        text-align: center;
+        padding: 12px;
+        font-weight: bold;
+        font-size: 14px;
+        line-height: 150%;
+        letter-spacing: 0%;
+        margin-bottom: 16px;
+        border-radius: 8px;
+
+        &:hover {
+          background-color: #C4F82A;
+          cursor: pointer;
+          color: #333333;
+        }
+      }
+    }
+  }
+`;
